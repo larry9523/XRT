8068deca84f5af833eca8d47e035851968d79fe5
abalasa <abalasa@xilinx.com>
2021-08-09 11:12:21 -0700

    Remove branch.json as it is no longer needed and add license to setuserenv.sh

--- build/docker/setUserEnv.sh
+++ build/docker/setUserEnv.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-#This Scripts sets the user env variables to run the gradle tasks in XRT IPU docker container
+#
+#  Copyright (C) 2021, Xilinx Inc
+#
+#  Apache License Verbiage
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# This Scripts sets the user env variables to run the gradle tasks in XRT IPU docker container
+# source setUserEnv.sh
 
 export XDOCK_USER=${USER}
 export XDOCK_UID=$(id -u ${USER})
