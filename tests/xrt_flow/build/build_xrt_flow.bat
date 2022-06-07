cc575c88abec92527191f1a664df01efe4b41920
Daniel Mok <daniel.mok@amd.com>
2022-06-07 17:40:20 -0400

    Update xrt_flow to use DPU with single config header

--- tests/xrt_flow/build/build_xrt_flow.bat
+++ tests/xrt_flow/build/build_xrt_flow.bat
@@ -2,7 +2,8 @@ REM Copyright(C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 
 @echo OFF
 
-REM Compile a separate test executable against each test case
+REM Build xrt_flow.exe
+REM With the unified config.h, there is no need to compile a separate executable per test case
 
 set CONFIGURATION=%1
 
@@ -14,31 +15,23 @@ if "%1"=="" (
 set XRT_FLOW_ROOT=%~dp0..
 set EXE_PATH=%XRT_FLOW_ROOT%\\x64\\%CONFIGURATION%
 
-REM Compile all the 1x4 DPU test cases
 REM See DPU\host_code\xrt_host_oo\1x4
 set DPU_TARGET=1x4
 
-for /D %%D in (%XRT_FLOW_ROOT%\..\..\DPU\host_code\xrt_host_oo\%DPU_TARGET%\*) do (
-    set TEST_CASE=%%~nxD
-    call :BUILD_TEST_CASE
-    if %ERRORLEVEL% neq 0 goto FAIL
-)
+call :BUILD_TEST_CASE
+if %ERRORLEVEL% neq 0 goto FAIL
 
 exit /b 0
 
 :BUILD_TEST_CASE
 echo ------------------------
-echo Build xrt_flow_%TEST_CASE%.exe
+echo Build xrt_flow.exe
 echo ------------------------
-msbuild %XRT_FLOW_ROOT%\\xrt_flow.sln /p:Configuration="%CONFIGURATION%" /p:Platform=x64 /p:XRT_FLOW_TEST_CASE="%TEST_CASE%" /p:DPU_TARGET="%DPU_TARGET%"
-if %ERRORLEVEL% neq 0 goto BUILD_TEST_CASE_EXIT
-
-REM Rename the xrt_flow executable to match with the test case
-copy /y %EXE_PATH%\\xrt_flow.exe %EXE_PATH%\\xrt_flow_%TEST_CASE%.exe
+msbuild %XRT_FLOW_ROOT%\\xrt_flow.sln /p:Configuration="%CONFIGURATION%" /p:Platform=x64 /p:DPU_TARGET="%DPU_TARGET%"
 if %ERRORLEVEL% neq 0 goto BUILD_TEST_CASE_EXIT
 :BUILD_TEST_CASE_EXIT
 exit /b %ERRORLEVEL%
 
 :FAIL
-echo Error when building or copying xrt_flow_%TEST_CASE%.exe
+echo Error when building or copying xrt_flow.exe
 exit /b %ERRORLEVEL%
--- tests/xrt_flow/build/postbuild.bat
+++ tests/xrt_flow/build/postbuild.bat
@@ -17,17 +17,18 @@ MKDIR %EXPORT%
 MKDIR %EXPORT%\bin
 
 REM Copy over xrt_flow.exe
-COPY /y %XRT_FLOW_ROOT%\x64\%CONFIGURATION%\xrt_flow.exe %EXPORT%\xrt_flow_%TEST_CASE%.exe
+COPY /y %XRT_FLOW_ROOT%\x64\%CONFIGURATION%\xrt_flow.exe %EXPORT%\xrt_flow.exe
 
 REM Copy over XRT-IPU DLLs
-COPY /y %XRT_IPU_ROOT%\build\W%CONFIGURATION%\xilinx\xrt\bin\*.dll %EXPORT%
-COPY /y %XRT_IPU_ROOT%\build\W%CONFIGURATION%\xilinx\xrt\bin\*.dll %EXPORT%\bin
+COPY /y %XRT_IPU_ROOT%\build\W%CONFIGURATION%\xilinx\xrt\bin\xrt_core.dll %EXPORT%\xrt_core.dll
+COPY /y %XRT_IPU_ROOT%\build\W%CONFIGURATION%\xilinx\xrt\bin\xrt_coreutil.dll %EXPORT%\xrt_coreutil.dll
+COPY /y %XRT_IPU_ROOT%\build\W%CONFIGURATION%\xilinx\xrt\bin\xrt_core.dll %EXPORT%\bin\xrt_core.dll
+COPY /y %XRT_IPU_ROOT%\build\W%CONFIGURATION%\xilinx\xrt\bin\xrt_coreutil.dll %EXPORT%\bin\xrt_coreutil.dll
 
 REM Copy over test case data folders
-XCOPY /e /i /y %DPU%\%DPU_TARGET%\%TEST_CASE% %EXPORT%\%TEST_CASE%
+XCOPY /e /i /y %DPU%\%DPU_TARGET% %EXPORT%\
 
 REM Copy over other files (XCL binary, xrt.ini, helper batch script)
-COPY /y %DPU%\..\4cmt_oo.xclbin %EXPORT%
 COPY /y %DPU%\..\1x3_oo.xclbin %EXPORT%
 COPY /y %XRT_FLOW_ROOT%\target\xrt.ini %EXPORT%
 COPY /y %XRT_FLOW_ROOT%\target\run_xrt_flow.bat %EXPORT%
